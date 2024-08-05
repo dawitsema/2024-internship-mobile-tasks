abb5a725ddc2f333a0eb4026e162f78251dcf37a
dawitsema <128223243+dawitsema@users.noreply.github.com>
2024-08-05 08:38:37 +0300

    Update to task 7 ProductCard.dart

--- on-boarding/task_6/lib/pages/ProductCard.dart
+++ on-boarding/task_6/lib/pages/ProductCard.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:task_6/pages/DetailsPage.dart';
+import 'DetailsPage.dart';
+import 'product.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({super.key});
+  final Product product;
+
+  const ProductCard({required this.product, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,14 +17,18 @@ class ProductCard extends StatelessWidget {
           Navigator.of(context).push(
             MaterialPageRoute(
               builder: (BuildContext context) {
-                return const Detailspage();
+                // Pass the actual product instance, not the type 'Product'
+                return Detailspage(
+                  product: product, // corrected this line
+                );
               },
             ),
           );
         },
         child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
           elevation: 3,
           child: Column(
             children: [
@@ -29,24 +36,24 @@ class ProductCard extends StatelessWidget {
                 borderRadius:
                     const BorderRadius.vertical(top: Radius.circular(15.0)),
                 child: Image.asset(
-                  'images/shoes.jpg',
-                  height: 250,
+                  product.imagePath,
+                  height: 150,
                   width: double.infinity,
                   fit: BoxFit.cover,
                 ),
               ),
-              const ListTile(
-                title: Text('Derby Leather Shoes'),
-                subtitle: Text('Men\'s shoe'),
+              ListTile(
+                title: Text(product.title),
+                subtitle: Text(product.catagory),
                 trailing: Column(
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Text('\$120'),
+                    Text(product.price),
                     Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         Icon(Icons.star, color: Colors.yellow, size: 20),
-                        Text('(4.0)'),
+                        Text(product.rating),
                       ],
                     ),
                   ],
